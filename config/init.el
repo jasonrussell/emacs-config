7ecab940fc5c7e55820fb42b8cb3535186dd429b
Jason Russell <jsonrssell@gmail.com>
2020-01-03 23:44:17 -0500

    Install helm if not exists in repository

--- config/init.el
+++ config/init.el
@@ -1,3 +1,12 @@
+(package-initialize)
+(require 'package)
+(add-to-list 'package-archives
+    '("melpa" . "https://melpa.org/packages/") t)
+
+(dolist (package '(use-package))
+   (unless (package-installed-p package)
+       (package-install package)))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -6,10 +15,14 @@
  '(custom-enabled-themes '(wombat))
  '(package-archives
    '(("melpa" . "https://melpa.org/packages/")
-     ("gnu" . "https://elpa.gnu.org/packages/"))))
+     ("gnu" . "https://elpa.gnu.org/packages/")))
+ '(package-selected-packages '(helm use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(use-package helm
+	     :ensure t)
