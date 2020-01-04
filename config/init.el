624a7611ce69114e990adeadf6e539203b12eadb
Jason Russell <jsonrssell@gmail.com>
2020-01-03 23:57:13 -0500

    Install and enable evil mode

--- config/init.el
+++ config/init.el
@@ -16,13 +16,20 @@
  '(package-archives
    '(("melpa" . "https://melpa.org/packages/")
      ("gnu" . "https://elpa.gnu.org/packages/")))
- '(package-selected-packages '(helm use-package)))
+ '(package-selected-packages '(evil helm use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; this is the way
+(use-package evil
+  :ensure t
+  :init
+  :config
+  (evil-mode 1))
 
 (use-package helm
   :ensure t
